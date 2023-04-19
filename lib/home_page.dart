cc8948d22558b042caefa8f80d232baef90dcc9c
mon842 <sayakbasak842@gmail.com>
2023-04-19 13:00:40 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days=3;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Cataloge app'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Welcome $days to Flutter'),
+          ),
+        ),
+        drawer: Drawer(),
+    );
+    
+  }
+}
